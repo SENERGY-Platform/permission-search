862798c8f06cad5fe6e92d3b5c3a452d02a41942
Ingo <roessner@infai.org>
2020-11-03 10:07:40 +0100

    test and fix config for device-groups

--- lib/devicegroups_test.go
+++ lib/devicegroups_test.go
@@ -1,7 +1,101 @@
 package lib
 
-import "testing"
+import (
+	"context"
+	"encoding/json"
+	"fmt"
+	"github.com/SENERGY-Platform/permission-search/lib/model"
+	"github.com/olivere/elastic/v7"
+	"reflect"
+	"sync"
+	"testing"
+	"time"
+)
 
-func TestDeviceGroups(t *testing.T) {
-	t.Error("not implemented")
+func TestDeviceGroup(t *testing.T) {
+	wg := &sync.WaitGroup{}
+	defer wg.Wait()
+	ctx, cancel := context.WithCancel(context.Background())
+	defer cancel()
+	_, q, w, err := getTestEnv(ctx, wg)
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+
+	resource := "device-groups"
+	_, err = q.GetClient().DeleteByQuery(resource).Query(elastic.NewMatchAllQuery()).Do(context.Background())
+	if err != nil {
+		t.Error(err)
+		return
+	}
+	_, err = q.GetClient().Flush().Index(resource).Do(context.Background())
+	if err != nil {
+		t.Error(err)
+		return
+	}
+	msg, cmd, err := getDeviceGroupTestObj("g1", map[string]interface{}{
+		"name":  "g1_name",
+		"image": "g1_image",
+		"foo":   "bar",
+		"bar":   42,
+	})
+	if err != nil {
+		t.Error(err)
+		return
+	}
+	err = w.UpdateFeatures(resource, msg, cmd)
+	if err != nil {
+		t.Error(err)
+		return
+	}
+
+	time.Sleep(2 * time.Second)
+
+	e, _, err := q.GetResourceEntry(resource, "g1")
+	if err != nil {
+		t.Error(err)
+		return
+	}
+	if !reflect.DeepEqual(e.Features["name"], "g1_name") {
+		t.Error(e)
+		return
+	}
+
+	result, err := q.GetOrderedListForUserOrGroup(resource, "testOwner", []string{"user"}, "r", "3", "0", "name", true)
+	if err != nil {
+		t.Error(err)
+		return
+	}
+	if len(result) != 1 {
+		t.Error(result)
+		return
+	}
+	if !reflect.DeepEqual(result[0]["name"], "g1_name") {
+		t.Error(result)
+		return
+	}
+	if !reflect.DeepEqual(result[0]["image"], "g1_image") {
+		t.Error(result)
+		return
+	}
+}
+
+func getDeviceGroupTestObj(id string, obj map[string]interface{}) (msg []byte, command model.CommandWrapper, err error) {
+	text := `{
+		"command": "PUT",
+		"id": "%s",
+		"owner": "testOwner",
+		"device_group": %s
+	}`
+	dtStr, err := json.Marshal(obj)
+	if err != nil {
+		return msg, command, err
+	}
+	msg = []byte(fmt.Sprintf(text, id, string(dtStr)))
+	err = json.Unmarshal(msg, &command)
+	if err != nil {
+		return msg, command, err
+	}
+	return msg, command, err
 }
