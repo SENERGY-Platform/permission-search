c4f4e16aa36f13b3dfd176859fffc4a912eed6fe
Ingo <roessner@infai.org>
2021-08-04 09:01:56 +0200

    set attributes type mapping

--- lib/devices_test.go
+++ lib/devices_test.go
@@ -3,6 +3,8 @@ package lib
 import (
 	"context"
 	"fmt"
+	"github.com/SENERGY-Platform/permission-search/lib/auth"
+	"github.com/SENERGY-Platform/permission-search/lib/model"
 	"github.com/olivere/elastic/v7"
 	"reflect"
 	"sync"
@@ -99,6 +101,109 @@ func TestReceiveDevice(t *testing.T) {
 	}
 	if !reflect.DeepEqual(result[1]["attributes"], attr2) {
 		t.Error(e)
+		return
+	}
+	t.Log(result)
+}
+
+func TestDeviceWithSpecialCharacterAttribute(t *testing.T) {
+	wg := &sync.WaitGroup{}
+	defer wg.Wait()
+	ctx, cancel := context.WithCancel(context.Background())
+	defer cancel()
+	_, q, w, err := getTestEnv(ctx, wg)
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+
+	resource := "devices"
+	_, err = q.GetClient().DeleteByQuery(resource).Query(elastic.NewMatchAllQuery()).Do(context.Background())
+	if err != nil {
+		t.Error(err)
+		return
+	}
+	_, err = q.GetClient().Flush().Index(resource).Do(context.Background())
+	if err != nil {
+		t.Error(err)
+		return
+	}
+
+	deviceMsg, deviceCmd, err := getDeviceTestObj("device1", map[string]interface{}{
+		"name": "device_name_1",
+	})
+	if err != nil {
+		t.Error(err)
+		return
+	}
+	err = w.UpdateFeatures(resource, deviceMsg, deviceCmd)
+	if err != nil {
+		t.Error(err)
+		return
+	}
+
+	attr2 := []interface{}{
+		map[string]interface{}{"key": "device-type/manufacturer", "value": "HEAT_COST_ALLOCATOR"},
+	}
+	deviceMsg, deviceCmd, err = getDeviceTestObj("device2", map[string]interface{}{
+		"name":       "device_name_2",
+		"attributes": attr2,
+	})
+	if err != nil {
+		t.Error(err)
+		return
+	}
+	err = w.UpdateFeatures(resource, deviceMsg, deviceCmd)
+	if err != nil {
+		t.Error(err)
+		return
+	}
+
+	time.Sleep(2 * time.Second)
+
+	filter, err := q.GetFilter(auth.Token{Sub: "testOwner", RealmAccess: map[string][]string{"roles": {}}}, model.Selection{
+		And: []model.Selection{
+			{
+				Condition: model.ConditionConfig{
+					Feature:   "features.attributes.key",
+					Operation: "==",
+					Value:     "device-type/manufacturer",
+				},
+			},
+			{
+				Condition: model.ConditionConfig{
+					Feature:   "features.attributes.value",
+					Operation: "==",
+					Value:     "HEAT_COST_ALLOCATOR",
+				},
+			},
+		},
+	})
+	if err != nil {
+		t.Error(err)
+		return
+	}
+	result, err := q.GetOrderedListForUserOrGroupWithSelection(
+		"devices",
+		"testOwner",
+		[]string{},
+		"r",
+		"10",
+		"0",
+		"name",
+		true,
+		filter)
+
+	if len(result) != 1 {
+		t.Error(result)
+		return
+	}
+	if !reflect.DeepEqual(result[0]["name"], "device_name_2") {
+		t.Error(result)
+		return
+	}
+	if !reflect.DeepEqual(result[0]["attributes"], attr2) {
+		t.Error(result)
 		return
 	}
 	t.Log(result)
