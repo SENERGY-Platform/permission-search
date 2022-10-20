930bab565fcb0f7276f2a2027b41e391117a4b9b
Ingo <roessner@infai.org>
2022-10-20 13:01:49 +0200

    add TestDeviceGroupAttributeListFilter test

--- lib/feature_concat_test.go
+++ lib/feature_concat_test.go
@@ -19,7 +19,9 @@ package lib
 import (
 	"context"
 	"fmt"
+	"github.com/SENERGY-Platform/permission-search/lib/auth"
 	"github.com/SENERGY-Platform/permission-search/lib/configuration"
+	"github.com/SENERGY-Platform/permission-search/lib/model"
 	"github.com/SENERGY-Platform/permission-search/lib/worker"
 	"github.com/olivere/elastic/v7"
 	"reflect"
@@ -134,6 +136,190 @@ func TestFeatureConcatWithDeviceGroup(t *testing.T) {
 	}
 	if !reflect.DeepEqual(e.Features["attribute_list"], []interface{}{"platform/generated=true", "platform/smart_service_task=foobar"}) {
 		t.Error(e)
+		return
+	}
+}
+
+func TestDeviceGroupAttributeListFilter(t *testing.T) {
+	wg := &sync.WaitGroup{}
+	defer wg.Wait()
+	ctx, cancel := context.WithCancel(context.Background())
+	defer cancel()
+	_, q, w, err := getTestEnv(ctx, wg, t)
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+
+	resource := "device-groups"
+	_, err = q.GetClient().DeleteByQuery(resource).Query(elastic.NewMatchAllQuery()).Do(context.Background())
+	if err != nil {
+		t.Error(err)
+		return
+	}
+	_, err = q.GetClient().Flush().Index(resource).Do(context.Background())
+	if err != nil {
+		t.Error(err)
+		return
+	}
+
+	msg, cmd, err := getDeviceGroupTestObj("g1", map[string]interface{}{
+		"name":       "g1_name_1",
+		"image":      "g1_image",
+		"device_ids": []string{"d1", "d2"},
+		"foo":        "bar",
+		"bar":        42,
+		"criteria": []interface{}{
+			map[string]interface{}{
+				"function_id":     "f1",
+				"aspect_id":       "a1",
+				"device_class_id": "",
+				"interaction":     "request",
+			},
+		},
+		"criteria_short": []interface{}{
+			"f1_a1__request",
+			"f1_a2__event",
+			"f2__dc1_request",
+			"f3__dc1_",
+		},
+		"attributes": []interface{}{
+			map[string]interface{}{
+				"key":    "platform/generated",
+				"value":  "true",
+				"origin": "smart-service",
+			},
+			map[string]interface{}{
+				"key":    "platform/smart_service_task",
+				"value":  "foobar",
+				"origin": "smart-service",
+			},
+		},
+	})
+	if err != nil {
+		t.Error(err)
+		return
+	}
+	err = w.UpdateFeatures(resource, msg, cmd)
+	if err != nil {
+		t.Error(err)
+		return
+	}
+
+	msg2, cmd2, err := getDeviceGroupTestObj("g2", map[string]interface{}{
+		"name":       "g2_name_2",
+		"image":      "g1_image",
+		"device_ids": []string{"d1", "d2"},
+		"foo":        "bar",
+		"bar":        42,
+		"criteria": []interface{}{
+			map[string]interface{}{
+				"function_id":     "f1",
+				"aspect_id":       "a1",
+				"device_class_id": "",
+				"interaction":     "request",
+			},
+		},
+		"criteria_short": []interface{}{
+			"f1_a1__request",
+			"f1_a2__event",
+			"f2__dc1_request",
+			"f3__dc1_",
+		},
+		"attributes": []interface{}{
+			map[string]interface{}{
+				"key":    "platform/smart_service_task",
+				"value":  "foobar",
+				"origin": "smart-service",
+			},
+		},
+	})
+	if err != nil {
+		t.Error(err)
+		return
+	}
+	err = w.UpdateFeatures(resource, msg2, cmd2)
+	if err != nil {
+		t.Error(err)
+		return
+	}
+
+	time.Sleep(2 * time.Second)
+
+	filter, err := q.GetFilter(auth.Token{}, model.Selection{
+		Condition: model.ConditionConfig{
+			Feature:   "features.attribute_list",
+			Operation: model.QueryEqualOperation,
+			Value:     "platform/generated=true",
+		},
+	})
+	if err != nil {
+		t.Error(err)
+		return
+	}
+
+	result, err := q.SearchOrderedListWithSelection(resource, "name", "testOwner", []string{"user"}, model.QueryListCommons{
+		Limit:  100,
+		Offset: 0,
+		Rights: "r",
+		SortBy: "name",
+	}, filter)
+
+	if err != nil {
+		t.Error(err)
+		return
+	}
+
+	if len(result) != 1 || result[0]["id"] != "g1" {
+		t.Error(result)
+		return
+	}
+
+	filter, err = q.GetFilter(auth.Token{}, model.Selection{
+		Not: &model.Selection{
+			Condition: model.ConditionConfig{
+				Feature:   "features.attribute_list",
+				Operation: model.QueryEqualOperation,
+				Value:     "platform/generated=true",
+			},
+		},
+	})
+	if err != nil {
+		t.Error(err)
+		return
+	}
+
+	result, err = q.SearchOrderedListWithSelection(resource, "name", "testOwner", []string{"user"}, model.QueryListCommons{
+		Limit:  100,
+		Offset: 0,
+		Rights: "r",
+		SortBy: "name",
+	}, filter)
+
+	if err != nil {
+		t.Error(err)
+		return
+	}
+
+	if len(result) != 1 || result[0]["id"] != "g2" {
+		t.Error(result)
+		return
+	}
+
+	result, err = q.GetOrderedListForUserOrGroupWithSelection(resource, "testOwner", []string{"user"}, model.QueryListCommons{
+		Limit:  100,
+		Offset: 0,
+		Rights: "r",
+		SortBy: "name",
+	}, filter)
+
+	if err != nil {
+		t.Error(err)
+		return
+	}
+
+	if len(result) != 1 || result[0]["id"] != "g2" {
+		t.Error(result)
 		return
 	}
 }
