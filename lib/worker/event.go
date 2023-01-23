40b981bf5f3c0230322f67a22ca0ecbaded92d98
Ingo <roessner@infai.org>
2023-01-23 14:41:28 +0100

    fix resource consumer

--- lib/worker/event.go
+++ lib/worker/event.go
@@ -68,7 +68,7 @@ func InitEventHandling(ctx context.Context, config configuration.Config, worker 
 		handlers[resource] = worker.GetResourceCommandHandler(resource)
 	}
 
-	err = kafka.NewConsumerWithMultipleTopics(ctx, config.KafkaUrl, config.GroupId, config.ResourceList, 1*time.Second, func(topic string, msg []byte) error {
+	err = kafka.NewConsumerWithMultipleTopics(ctx, config.KafkaUrl, config.GroupId, resourceTopics, 1*time.Second, func(topic string, msg []byte) error {
 		f, ok := handlers[topic]
 		if !ok {
 			log.Println("ERROR: unknown topic handler ", topic)
